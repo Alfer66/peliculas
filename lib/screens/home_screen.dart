265eb7425ed624715211863c334c2c5c2a6b0ee1
DESKTOP-TE715TO\alfer <alfer>
2023-05-14 22:39:00 -0600

    2023_05_14_2238_ARV
    Avance del curso de Flutter.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
 import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+import 'package:peliculas/search/search_delegate.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
+    print(moviesProvider.onDisplayMovies);
     return Scaffold(
         appBar: AppBar(
-          title: Text('Películas en cines'),
+          title: const Text('Películas en cines'),
           elevation: 0,
           actions: [
             IconButton(
               icon: const Icon(Icons.search_outlined),
-              onPressed: () {},
+              onPressed: () =>
+                  showSearch(context: context, delegate: MovieSearchDelegate()),
             )
           ],
         ),
-        body: Column(
-          children: [
-            CardSwiper(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              //Tarjetas principales
+              CardSwiper(movies: moviesProvider.onDisplayMovies),
 
-            //Listado horizontal de películas
-          ],
+              //Slider de peliculas
+              MovieSlider(
+                movies: moviesProvider.popularMovies,
+                title: 'Populares',
+                onNextPage: () => moviesProvider.getPopularMovies(),
+              )
+            ],
+          ),
         ));
   }
 }
